c22103e4996b0338c2976c4bdef8559de3c96bc2
Rob Dimsdale <rdimsdale@pivotal.io>
2016-05-24 20:10:31 -0700

    Backfill custom error messages test in command error handler.
    [#119507367]

--- cmd/pivnet/errors/error_handler_test.go
+++ cmd/pivnet/errors/error_handler_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"github.com/pivotal-cf-experimental/go-pivnet"
 	"github.com/pivotal-cf-experimental/go-pivnet/cmd/pivnet/errors"
 	"github.com/pivotal-cf-experimental/go-pivnet/cmd/pivnet/printer"
 	"github.com/pivotal-cf-experimental/go-pivnet/cmd/pivnet/printer/printerfakes"
@@ -15,32 +16,37 @@ var _ = Describe("ErrorHandler", func() {
 		fakePrinter *printerfakes.FakePrinter
 
 		errorHandler errors.ErrorHandler
+		inputErr     error
 	)
 
 	BeforeEach(func() {
 		fakePrinter = &printerfakes.FakePrinter{}
 
 		errorHandler = errors.NewErrorHandler(printer.PrintAsTable, fakePrinter)
+
+		inputErr = fmt.Errorf("some error")
 	})
 
 	It("returns provided error", func() {
-		inputErr := fmt.Errorf("some error")
-
 		err := errorHandler.HandleError(inputErr)
+
 		Expect(err).To(Equal(inputErr))
 	})
 
 	It("writes to printer", func() {
-		inputErr := fmt.Errorf("some error")
-
 		_ = errorHandler.HandleError(inputErr)
 
 		Expect(fakePrinter.PrintlnCallCount()).To(Equal(1))
 	})
 
 	Context("when the error is nil", func() {
+		BeforeEach(func() {
+			inputErr = nil
+		})
+
 		It("returns nil", func() {
-			err := errorHandler.HandleError(nil)
+			err := errorHandler.HandleError(inputErr)
+
 			Expect(err).NotTo(HaveOccurred())
 		})
 
@@ -59,8 +65,6 @@ var _ = Describe("ErrorHandler", func() {
 		})
 
 		It("writes to printer", func() {
-			inputErr := fmt.Errorf("some error")
-
 			_ = errorHandler.HandleError(inputErr)
 
 			Expect(fakePrinter.PrintJSONCallCount()).To(Equal(1))
@@ -73,11 +77,37 @@ var _ = Describe("ErrorHandler", func() {
 		})
 
 		It("writes to printer", func() {
-			inputErr := fmt.Errorf("some error")
-
 			_ = errorHandler.HandleError(inputErr)
 
 			Expect(fakePrinter.PrintYAMLCallCount()).To(Equal(1))
+		})
+	})
+
+	Describe("Handling specific Pivnet errors", func() {
+		Describe("pivnet.ErrUnauthorized", func() {
+			BeforeEach(func() {
+				inputErr = pivnet.ErrUnauthorized{}
+			})
+
+			It("retuns custom message", func() {
+				_ = errorHandler.HandleError(inputErr)
+
+				Expect(fakePrinter.PrintlnCallCount()).To(Equal(1))
+				Expect(fakePrinter.PrintlnArgsForCall(0)).To(Equal("Please log in first"))
+			})
+		})
+
+		Describe("pivnet.ErrNotFound", func() {
+			BeforeEach(func() {
+				inputErr = pivnet.ErrNotFound{}
+			})
+
+			It("retuns custom message", func() {
+				_ = errorHandler.HandleError(inputErr)
+
+				Expect(fakePrinter.PrintlnCallCount()).To(Equal(1))
+				Expect(fakePrinter.PrintlnArgsForCall(0)).To(Equal("Not found"))
+			})
 		})
 	})
 })
