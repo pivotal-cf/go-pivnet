f5c559ce1e582f723fe87f97cecb10e67652701f
Rob Dimsdale-Zucker <rdimsdale@pivotal.io>
2016-12-12 08:27:17 -0800

    Add test to verify progress bar is race-free. [#135897297]

---
+++ download/progress_test.go
@@ -0,0 +1,44 @@
+package download_test
+
+import (
+	"io/ioutil"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+
+	"github.com/pivotal-cf/go-pivnet/download"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Progress", func() {
+	var (
+		b download.Bar
+	)
+
+	BeforeEach(func() {
+		b = download.NewBar()
+	})
+
+	It("handles concurrent writes without racing", func() {
+		total := 10
+
+		b.SetTotal(int64(total))
+		b.Output = ioutil.Discard
+		b.Kickoff()
+
+		var g errgroup.Group
+		for i := 0; i < total; i++ {
+			g.Go(func() error {
+				time.Sleep(10 * time.Millisecond)
+
+				_, err := b.Write([]byte("a"))
+				return err
+			})
+		}
+
+		err := g.Wait()
+		Expect(err).NotTo(HaveOccurred())
+	})
+})
