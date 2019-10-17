255c30884cf40b230ae9152e46565ffdbf488ecc
Behrouz Soroushian <bsoroushian@pivotal.io>
2019-10-17 16:16:19 -0400

    Implement slug alias endpoint
    - the endpoint provides old and current slugs of a product
    
    [#168550973]
    
    Signed-off-by: Behrouz Soroushian <bsoroushian@pivotal.io>

--- products.go
+++ products.go
@@ -25,6 +25,11 @@ type Product struct {
 	InstallsOnPks bool         `json:"installs_on_pks,omitempty" yaml:"installs_on_pks,omitempty"`
 }
 
+type SlugAliasResponse struct {
+	Slugs       []string `json:"slugs" yaml:"slugs"`
+	CurrentSlug string   `json:"current_slug" yaml:"current_slug"`
+}
+
 type ProductsResponse struct {
 	Products []Product `json:"products,omitempty"`
 }
@@ -70,6 +75,29 @@ func (p ProductsService) Get(slug string) (Product, error) {
 	err = json.NewDecoder(resp.Body).Decode(&response)
 	if err != nil {
 		return Product{}, err
+	}
+
+	return response, nil
+}
+
+func (p ProductsService) SlugAlias(slug string) (SlugAliasResponse, error) {
+	url := fmt.Sprintf("/products/%s/slug_alias", slug)
+
+	var response SlugAliasResponse
+	resp, err := p.client.MakeRequest(
+		"GET",
+		url,
+		http.StatusOK,
+		nil,
+	)
+	if err != nil {
+		return SlugAliasResponse{}, err
+	}
+	defer resp.Body.Close()
+
+	err = json.NewDecoder(resp.Body).Decode(&response)
+	if err != nil {
+		return SlugAliasResponse{}, err
 	}
 
 	return response, nil
